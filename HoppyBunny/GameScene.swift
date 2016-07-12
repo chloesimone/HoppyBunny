1528566c2e7144d9ea57c78baeba893632bce419
Linda Brown <lindabrown@Lindas-MacBook-Air.local>
2016-07-12 12:05:39 -0700

    My first commit

---
+++ HoppyBunny/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  HoppyBunny
+//
+//  Created by Linda Brown on 7/11/16.
+//  Copyright (c) 2016 Linda Brown. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Set up your scene here */
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        /* Called when a touch begins */
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
